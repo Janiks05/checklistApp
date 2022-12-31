3e39ebb014aae20e41d6da8eaf791ccc629f5ef0
Janiks05 <73661508+Janiks05@users.noreply.github.com>
2022-12-31 14:14:43 +0100

    headline + api anpassungen

--- js/getChecklist.js
+++ js/getChecklist.js
@@ -3,6 +3,7 @@ const footer = document.querySelector('#footer')
 const main = document.querySelector('#main')
 
 let checklist = document.querySelector('.checklist')
+let checklistHeadline = document.querySelector('#checklist-headline')
 
 //gibt checkliste mit daten der API aus 
 function getChecklist(checklistType) {
@@ -18,31 +19,38 @@ function getChecklist(checklistType) {
         .then(res => {
             return res.json();
         })
-        .then(data => console.log(data.forEach(task => {
-            let markup;
-            if (task.length >= 2) {
-                for (let i = 0; i < task.length; i++) {
-                    if (i == 0) {
-                        markup = `<li>${task[i]}</li>`
-                    } else {
-                        markup = `<ol><li>${task[i]}</li></ol>` //soll eingerückt sein
+
+        .then(data => {
+            console.log(data)
+            checklistHeadline.innerHTML = data.name
+            data.checklistItems.forEach(task => {
+                let markup;
+                if (task.length >= 2) {
+                    for (let i = 0; i < task.length; i++) {
+                        if (i == 0) {
+                            markup = `<li>${task[i]}</li>`
+                        } else {
+                            markup = `<ol><li>${task[i]}</li></ol>` //soll eingerückt sein
+                        }
+                        document.querySelector('ul').innerHTML += markup
                     }
-                    document.querySelector('ul').innerHTML += markup
+                } else {
+                    markup = `<li>${task[0]}</li>`
+                    document.querySelector('#checklist-list').innerHTML += markup
+
                 }
-            } else {
-                markup = `<li>${task[0]}</li>`
-                document.querySelector('#checklist-list').innerHTML += markup
 
-            }
+            })
+        })
 
-        })));
+
 }
 
 //schließt checkliste --x zurück zur Hauptseite
 //animation für die ausblendende Checkliste
 function removeChecklist() {
     checklist.classList.add('removed-checklist')
-    setTimeout(()=>{checklist.classList.add('invisible')}, 100)
+    setTimeout(() => { checklist.classList.add('invisible') }, 100)
     footer.classList.remove('invisible')
     main.classList.remove('invisible')
 
