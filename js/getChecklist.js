92ffca2da769d8db29ce19f72d0929db2cfac4aa
Janiks05 <73661508+Janiks05@users.noreply.github.com>
2023-01-29 20:47:54 +0100

    ghz

--- js/getChecklist.js
+++ js/getChecklist.js
@@ -12,18 +12,30 @@ function getAndDisplayChecklistMainFrame() {
             return res.json()
         })
         .then(data => {
-            console.log(data);
             for (let i = 0; i < data.length; i++) {
-                dailys.innerHTML += `
-            <div class="daily" onclick="getChecklist('${data[i][0]}')">
-                <header>
-                    <h2>${data[i][1]}</h2>
-                    <div class="del-btn"></div>
-                </header>
 
-                <img src="${data[i][2]}" alt="">
-            </div>
+                if (data[i][0] !== '') {
+                    console.log(data[i]);
+
+                    dailys.innerHTML += `
+                    <div class="daily" onclick="getChecklist('${data[i][0]}')">
+                        <header>
+                            <h2>${data[i][1]}</h2>
+                            <div class="del-btn"></div>
+                        </header>
+                        
+                        <img src="${data[i][2]}" alt="">
+                    </div>
             `
+
+                }
+                else {
+                    dailys.innerHTML += `
+                    <div class="emptydaily">
+                    
+                    </div>
+                    `
+                }
             }
         })
 }
